59f02d7a92bd3e4e3a8615b1ec13770bc1958383
Barun355 <baruntiwary620@gmail.com>
2023-11-29 01:09:07 +0530

    utils code disscussion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 // require('dotenv').config({path: './env'})
 import dotenv from 'dotenv'
+import { app } from './app.js'
 import connectDB from "./db/index.js"
 
 dotenv.config({
     path: './env'
 })
 
+
+
 connectDB()
+.then(() => {
+    app.on("error", (err) => {
+        console.error("App failure ::", err)
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    });
+})
+.catch(err => {
+    console.error("DB Connection failed :: ", err)
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+
+const asyncHandlerPromise = (handler) => {
+    (req, res, next) => {
+        Promise.resolve(() => handler(req, res, next)).catch(err => next(err))
+    }
+}
+
+
+const asyncHandlerTryCatch = (func) => async (req, res, next) => {
+    try {
+        return await func(req, res, next)
+    } catch (error) {
+        req.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+
+
+export { asyncHandlerPromise, asyncHandlerTryCatch }
